d2b04fc1ab27a0abed52e47cf7f420ac9e74ecbb
Ramziddin7 <rustamovramziddin7@gmail.com>
2024-07-22 03:25:10 +0200

    first commit

---
+++ src/main/java/dasturlash/uz/config/ApplicationConfig.java
@@ -0,0 +1,32 @@
+package dasturlash.uz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages="dasturlash.uz")
+public class ApplicationConfig {
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/javaDb");
+        dataSource.setUsername("javaDbUser");
+        dataSource.setPassword("root");
+
+        return dataSource;
+    }
+}
